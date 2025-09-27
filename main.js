45cabf090b5ae9bd63838ebcfd8ed90eb90a1004
theskiesreflection <echoesandrain@gmail.com>
2025-09-27 17:36:31 +0100

    Really create the website

---
+++ main.js
@@ -0,0 +1,45 @@
+// vscode please let me access the top of my code
+function setElementHTML(id, payload){
+  document.getElementById(id).innerHTML = payload
+}
+function getElementContent(id){
+  return document.getElementById(id).value
+}
+let correctLetter = ""
+let started = 0
+function generate(){
+  let letters = ["a", "b", "c", "d", "e", "f", "g", "h",
+              "i", "j", "k", "l", "m", "n", "o", "p",
+               "q", "r", "s", "t", "u", "v", "w", "x", "y"]
+  let letterAmount = getElementContent("uniquechar")
+  let lineBreakInterval = getElementContent("linebreak")
+  let duplicate = Math.ceil(Math.random()*letterAmount)
+  correctLetter = letters[duplicate-1]
+  let newLetters = ["", "", "", "", "","", "", "", "", "",
+    "", "", "", "", "","", "", "", "", "",
+    "", "", "", "", ""
+  ]
+  for(i=0;i<letterAmount-1;i++){
+    newLetters[i] = letters[i]
+  }
+  newLetters[letterAmount-1] = letters[duplicate-1]
+  console.log(newLetters)
+  let rows = Math.ceil(letterAmount/lineBreakInterval)
+  let payload = ""
+  for(i=1;i<=rows;i++){
+    for(j=1;j<=lineBreakInterval;j++){
+      if(newLetters.length >= 1){
+        let rand = Math.floor(Math.random()*newLetters.length)
+        payload += newLetters[rand]
+        newLetters.splice(rand, 1)
+      }
+    }
+    payload += "<br>"
+  }
+  setElementHTML("payload", payload)
+  started = Date.now()
+}
+function submit(){
+  let time = (Date.now() - started)/1000
+  setElementHTML("lasttime", time.toFixed(3))
+}
