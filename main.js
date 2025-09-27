91b2a07698818e356fcc9901164671f6cf3d1ce3
theskiesreflection <echoesandrain@gmail.com>
2025-09-27 18:06:20 +0100

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function generate(){
                "q", "r", "s", "t", "u", "v", "w", "x", "y"]
   let letterAmount = getElementContent("uniquechar")
   let lineBreakInterval = getElementContent("linebreak")
-  let duplicate = Math.ceil(Math.random()*(letterAmount-1))
+  let duplicate = Math.ceil(Math.random()*(letterAmount))
   correctLetter = letters[duplicate-1]
   let newLetters = ["", "", "", "", "","", "", "", "", "",
     "", "", "", "", "","", "", "", "", "",
@@ -46,6 +46,7 @@ function submit(){
   let time = (Date.now() - started)/1000
   setElementHTML("lasttime", time.toFixed(3))
 }
+
 
 
 
